3f5b79a20bc85b07be896b29516f5dc3669f2c87
Thomas Grillo <thomas.a.grillo@gmail.com>
2016-06-23 14:36:46 +0200

    Corrections dans le protocole Commentaires Doxygen de Djoe

--- communication/com.cpp
+++ communication/com.cpp
@@ -1,128 +1,137 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
-#include <uuid/uuid.h>
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
+#include <cstdlib>
 #include "com.h"
 
-#define TAILLE_TRAME_MAX 32;
-#define TIMEOUT_CHEF 2000; //secondes
-#define TIMEOUT_INIT 60000; //secondes
+#define TAILLE_TRAME_MAX 32
+#define TIMEOUT_CHEF 2000 //secondesss
+#define TIMEOUT_INIT 60000 //secondes
 
-boolean Com::chefRequete(int ID, int * num) {
-  sprintf(buffer, "%02d", TYPE_CHEF_REQ);
-  sprintf(buffer+strlen(buffer), "%05d", ID);
-  sendMsg(buffer);
-
-  unsigned long start = millis();
-  unsigned long end;
-  do {
-    recvMsg(buffer); // Lance recv en boucle jusqu'au timeout
-    end = millis();
-
-    if(end - start >= TIMEOUT_CHEF) break;
-  } while(!strlen(buffer));
-
-  if(strlen(buffer) > 0) (
-    char strNum[3];
-    char strType[3];
-    strncpy(strNum, buffer, 2);
-    strncpy(strType, buffer+2, 2);
-
-    if(atoi(strType) == TYPE_CHEF_REP) {
-      char strID[6];
-      strncpy(strID, buffer+4, 5);
-
-      if(ID == atoi(id)) {
-        *num = atoi(strNum);
-        return true;
-      }
+bool Com::chefRequete(int ID, int *num) {
+    char buffer[RH_ASK_MAX_MESSAGE_LEN];
+    for (int i = 0; i < RH_ASK_MAX_MESSAGE_LEN; ++i) {
+        buffer[i] = 0;
     }
-  }
-  return false;
-}
+    sprintf(buffer, "00");
+    sprintf(buffer + strlen(buffer), "%02d", TYPE_CHEF_REQ);
+    sprintf(buffer + strlen(buffer), "%010d", ID);
+    sendMsg(buffer);
 
-void Com::chefReponse() {
-  sprintf(buffer, "%02d", numAssigne++);
-  sprintf(buffer+strlen(buffer), "%02d", TYPE_CHEF_REP);
-
-  sendMsg(buffer);
-}
-
-void Com::setup()
-{
-  Serial.begin(9600);  // Debugging only
-  driver.setModeRx();
-  if (!driver.init())
-  {
-    Serial.println("init failed");
-  }
-  randomSeed(analogRead(0));
-}
-
-void Com::init() {
-  ID = genID();
-  if(!chefRequete(ID)) {
-    isChef = true;
     unsigned long start = millis();
     unsigned long end;
-    do{
-      do {
+    do {
         recvMsg(buffer); // Lance recv en boucle jusqu'au timeout
         end = millis();
 
-        if(end - start >= TIMEOUT_CHEF) break;
-      } while(!strlen(buffer));
-      end = millis();
-      chefReponse();
-    } while(end - start < TIMEOUT_CHEF);
-  }
+        if (end - start >= TIMEOUT_CHEF) break;
+    } while (!strlen(buffer));
+
+    if (strlen(buffer) > 0) {
+        msg_header_t header;
+        getHeaderMessage(&header, buffer);
+
+        if (header.type == TYPE_CHEF_REP) {
+
+            if (ID == getID(buffer)) {
+                *num = header.dest;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void Com::chefReponse(unsigned long id) {
+    char buffer[RH_ASK_MAX_MESSAGE_LEN];
+
+    sprintf(buffer, "%02d", numAssigne++);
+    sprintf(buffer + strlen(buffer), "%02d", TYPE_CHEF_REP);
+    sprintf(buffer + strlen(buffer), "%010d", id);
+
+    sendMsg(buffer);
+}
+
+void Com::setup() {
+    Serial.begin(9600);  // Debugging only
+    driver.setModeRx();
+    if (!driver.init()) {
+        Serial.println("init failed");
+    }
+    randomSeed(analogRead(0));
+}
+
+void Com::init() {
+    char buffer[RH_ASK_MAX_MESSAGE_LEN];
+    char isChef = false;
+    ID = genID();
+
+    if (!chefRequete(ID, &num)) {
+        isChef = true;
+        unsigned long start = millis();
+        unsigned long end;
+        do {
+            do {
+                recvMsg(buffer); // Lance recv en boucle jusqu'au timeout
+                end = millis();
+
+                if (end - start >= TIMEOUT_CHEF) break;
+            } while (!strlen(buffer));
+            end = millis();
+
+            msg_header_t header;
+            getHeaderMessage(&header, buffer);
+            if(header.type == TYPE_CHEF_REQ) {
+                chefReponse(getID(buffer));
+            }
+        } while (end - start < TIMEOUT_CHEF);
+    }
 }
 
 void Com::marche_avant(int vitesse) {
-  char buffer[100] = {0};
-  sprintf(buffer, "0");
-  sprintf(buffer+strlen(buffer), "%02d", TYPE_AVANT);
-  sprintf(buffer+strlen(buffer), "%02d", vitesse);
-  sendMsg(buffer);
+    char buffer[100] = {0};
+    sprintf(buffer, "0");
+    sprintf(buffer + strlen(buffer), "%02d", TYPE_AVANT);
+    sprintf(buffer + strlen(buffer), "%02d", vitesse);
+    sendMsg(buffer);
 }
 
 void Com::stop() {
-  char buffer[100] = {0};
-  sprintf(buffer, "0");
-  sprintf(buffer+strlen(buffer), "%02d", TYPE_STOP);
-  sendMsg(buffer);
+    char buffer[100] = {0};
+    sprintf(buffer, "0");
+    sprintf(buffer + strlen(buffer), "%02d", TYPE_STOP);
+    sendMsg(buffer);
 }
 
 void Com::droite() {
-  char buffer[100] = {0};
-  sprintf(buffer, "0");
-  sprintf(buffer+strlen(buffer), "%02d", TYPE_DROITE);
-  sendMsg(buffer);
+    char buffer[100] = {0};
+    sprintf(buffer, "0");
+    sprintf(buffer + strlen(buffer), "%02d", TYPE_DROITE);
+    sendMsg(buffer);
 }
 
 void Com::gauche() {
-  char buffer[100] = {0};
-  sprintf(buffer, "0");
-  sprintf(buffer+strlen(buffer), "%02d", TYPE_GAUCHE);
-  sendMsg(buffer);
+    char buffer[100] = {0};
+    sprintf(buffer, "0");
+    sprintf(buffer + strlen(buffer), "%02d", TYPE_GAUCHE);
+    sendMsg(buffer);
 }
 
-void Com::com(uint8_t * etat) {
-  char buffer[RH_ASK_MAX_MESSAGE_LEN];
+void Com::com(uint8_t *type) {
+    char buffer[RH_ASK_MAX_MESSAGE_LEN];
 
-  if(recvMsg(&buffer)) {
-    msg_header_t header;
-    getHeaderMessage(&header, buffer);
-    if(header.dest == num || header.dest == 0) {
-      switch(header.type) {
-        case TYPE_AVANT:
-        case TYPE_STOP:
-        case TYPE_DROITE:
-        case TYPE_GAUCHE:
-          *etat = header.type;
-      }
+    if (recvMsg(buffer)) {
+        msg_header_t header;
+        getHeaderMessage(&header, buffer);
+        if (header.dest == num || header.dest == 0) {
+            switch (header.type) {
+                case TYPE_AVANT:
+                case TYPE_STOP:
+                case TYPE_DROITE:
+                case TYPE_GAUCHE:
+                    *type = header.type;
+            }
+        }
     }
-  }
 }
--- communication/com.h
+++ communication/com.h
@@ -1,73 +1,189 @@
 #ifndef COM_H
 #define COM_H
 
+#include <RH_ASK.h>
+#include <SPI.h>
 
 typedef struct {
-  uint8_t type;
-  uint8_t dest;
+    uint8_t dest;
+    uint8_t type;
 } msg_header_t;
 
-class Com
-{
-  RH_ASK driver(2000,13,12); // reception sur 13 , transmission sur 12
-  int ID = -1;
-  int num = -1;
-  int numAssigne = 1; // seulement pour le chef
-  boolean chef = false;
+class Com {
+    RH_ASK driver(2000,13,12); // reception sur 13 , transmission sur 12
+    int ID = -1;
+    int num = -1;
+    int numAssigne = 1; // seulement pour le chef
+    bool chef = false;
 
-  char getHeaderMessage(msg_header_t * header, char * buffer) {
-    char strType[3];
-    char strDest[3];
-    strncpy(strDest, buffer, 2);
-    strncpy(strType, buffer+2, 2);
-    header->dest = atoi(strDest);
-    header->type = atoi(strType);
-  }
-
-  // Fonction de transmission
-  void sendMsg(char* msg)
-  {
-    driver.setModeTx();
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-  }
-
-  // Fonction de reception
-  uint8_t recvMsg(char * buffer) // la taille du buffer ne doit pas exceder RH_ASK_MAX_MESSAGE_LEN
-  {
-    driver.setModeRx();
-    uint8_t buflen = -1;
-    char message[8];
-    if (driver.recv((uint8_t *)buffer, &buflen)) // Non-blocking
-    {
-      for (int i = 0 ; i < buflen ; i++)
-      {
-        message[i] = buf[i];
-        Serial.print(message[i]);
-      }
-      Serial.println();
+    /*!
+     * \fn private  char Com::getHeaderMessage(msg_header_t * header, char * buffer);
+     * \brief Extrait le Header du message.
+     *
+     * \param header : Valeur de retour contenant les information du header
+     * \param buffer : buffer contenant le massage
+     * \return char
+     */
+    char getHeaderMessage(msg_header_t *header, char *buffer) {
+        char strType[3];
+        char strDest[3];
+        strncpy(strDest, buffer, 2);
+        strncpy(strType, buffer + 2, 2);
+        header->dest = atoi(strDest);
+        header->type = atoi(strType);
     }
-    return buflen;
-  }
 
-  unsigned long genID() {
-    return random(30000);
-  }
+    unsigned long getID(char * buffer) {
+        char strID[11] = {0};
+        strncpy(strID, buffer + 4, 10);
+        return strtoul(strID, NULL, 0);
+    }
 
-  boolean chefRequete(int ID, int * num);
-  void chefReponse();
+
+    /*!
+     * \fn private  void Com::sendMsg(char* msg);
+     * \brief Envoie un message en boardcast
+     *
+     * \param msg : le contenue du message à envoyer
+     * \return void
+     */
+    void sendMsg(char *msg) {
+        driver.setModeTx();
+        driver.send((uint8_t *) msg, strlen(msg));
+        driver.waitPacketSent();
+    }
+
+
+    /*!
+     * \fn private  uint8_t Com::recvMsg(char * buffer)
+     * \brief Ecoute et reçois les messages
+     * la taille du buffer ne doit pas dépasser RH_ASK_MAX_MESSAGE_LEN
+     *
+     * \param buffer : le buffer au sera stocker le contenue du message reçus
+     * \return uint8_t la taille du contenue du message
+     */
+    uint8_t recvMsg(char *buffer) // la taille du buffer ne doit pas exceder RH_ASK_MAX_MESSAGE_LEN
+    {
+        driver.setModeRx();
+        uint8_t buflen = -1;
+        char message[8];
+        if (driver.recv((uint8_t *) buffer, &buflen)) // Non-blocking
+        {
+            for (int i = 0; i < buflen; i++) {
+                message[i] = buf[i];
+                Serial.print(message[i]);
+            }
+            Serial.println();
+        }
+        return buflen;
+    }
+
+    /*!
+     * \fn private  unsigned long Com::genID();
+     * \brief sert à generé un ID unique qui identifira le robot
+     *
+     * \return unsigned long un nombre compris entre 0 et 30000
+     */
+    unsigned long genID() {
+        return millis();
+    }
+
+    /*!
+     * \fn private  boolean Com::chefRequete(int ID, int * num);
+     * \brief Envoie un message pour savoir si un chef à déjà était élu, et si ou, récupére le nouvelle ID unique
+     *
+     * \param ID : l'ID unique du robot lors de son lancement
+     * \param num : l'ID unique du robot assignée par le chef
+     * \return boolean true si un chef est déjà élus
+     */
+    bool chefRequete(int ID, int *num);
+
+
+    /*!
+     * \fn private  void Com::chefReponse();
+     * \brief La réponse du chef à chefRequete, contient le nouvelle ID unique
+     *
+     * \return void
+     */
+    void chefReponse();
+
 public:
-  Com();
+    enum {
+        TYPE_CHEF_REQ,
+        TYPE_CHEF_REP,
+        TYPE_AVANT,
+        TYPE_STOP,
+        TYPE_DROITE,
+        TYPE_GAUCHE
+    };
 
-  void setup();
-  void init();
-  void marche_avant(int vitesse);
-  void stop();
-  void droite();
-  void gauche();
-  void com(uint8_t * etat);
+    Com();
 
-  boolean isChef() { return chef; }
+    /*!
+     * \fn public  void Com::setup();
+     * \brief Instancie le driver du communication
+     *
+     * \return void
+     */
+    void setup();
+
+    /*!
+     * \fn public  void Com::init();
+     * \brief Initialise les ids des robot et permet d'élire un chef
+     *
+     * \return void
+     */
+    void init();
+
+    /*!
+     * \fn public  void Com::marche_avant(int vitesse);
+     * \brief Envoie un message pour ordonner d'avancer au robot
+     *
+     * \param vitesse :
+     * \return void
+     */
+    void marche_avant(int vitesse);
+
+    /*!
+     * \fn public  void Com::stop();
+     * \brief Ordonne l'arrêt des robot
+     *
+     * \return void
+     */
+    void stop();
+
+    /*!
+     * \fn public  void Com::droite();
+     * \brief Ordonne de tourner à droite
+     *
+     * \return void
+     */
+    void droite();
+
+    /*!
+     * \fn public  void Com::gauche();
+     * \brief Odonne de tourner à gauche
+     *
+     * \return void
+     */
+    void gauche();
+
+    /*
+     * \fn public  void Com::com(uint8_t * type);
+     * \brief Reçois les message contenant les ordres de marche
+     *
+     * \param type :
+     * \return void
+     */
+    void com(uint8_t *type);
+
+    /*!
+     * \fn public  boolean Com::isChef();
+     * \brief Permet de savoit si le robot est le chef
+     *
+     * \return boolean
+     */
+    bool isChef() { return chef; }
 }
 
 #endif // COM_H
